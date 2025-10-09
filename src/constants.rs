fe0416047298d3957b73219d244b177a301cdf94
Retype15 <reynierramos280@gmail.com>
2025-10-09 16:03:34 -0400

    chore: configure GPG signing

--- src/constants.rs
+++ src/constants.rs
@@ -12,4 +12,5 @@ pub const GLOBAL_INDEX_FILENAME: &str = "index.bin";
 /// The name of the file containing a project's identity and references.
 pub const PROJECT_REF_FILENAME: &str = "project_ref.bin";
 
+/// -
 pub const MAX_RECURSION_DEPTH: u32 = 32;
